f8792e1edc6ee4fbe6c702a534b581533f5702c8
Jasper Van Proeyen <trianglejuice@mac.com>
2017-03-14 14:32:34 +0100

    Removed unnecessary require

--- gulp/copy.js
+++ gulp/copy.js
@@ -6,7 +6,6 @@
 // - https://www.npmjs.com/package/merge-stream
 
 var gulp = require('gulp');
-var plumber = require('gulp-plumber');
 var rename = require('gulp-rename');
 var merge = require('merge-stream');
 
