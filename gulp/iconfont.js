ab31b0cc70e82fbe1e8b1bdf91b672068911fe62
Tom Ceuppens <t-c@live.be>
2019-09-16 15:52:17 +0200

    To single quote

--- gulp/iconfont.js
+++ gulp/iconfont.js
@@ -7,22 +7,24 @@
 // - https://www.npmjs.org/package/lodash
 // - https://www.npmjs.org/package/gulp-rename
 
-var gulp = require("gulp");
+var gulp = require('gulp');
 
-var imagemin = require("gulp-imagemin"),
-    iconfont = require("gulp-iconfont"),
-    consolidate = require("gulp-consolidate"),
-    rename = require("gulp-rename"),
-    del = require("del");
+var imagemin = require('gulp-imagemin'),
+    iconfont = require('gulp-iconfont'),
+    consolidate = require('gulp-consolidate'),
+    rename = require('gulp-rename'),
+    del = require('del');
 
-gulp.task("icon-font", function() {
-    var fontName = "Antwerpen_Icons";
+gulp.task('icon-font', function() {
+    var fontName = 'Antwerpen_Icons';
 
     // Set svg-sources, optimize svg
     // and start creating the font
 
+    // disable `convertPathDate` and `mergePaths`
+    // REASON: merge paths together isn't that great of an idea especialy when applying different fill colors to seperate paths
     return gulp
-        .src("src/icons/*.svg")
+        .src('src/icons/*.svg')
         .pipe(
             imagemin([
                 imagemin.svgo({
@@ -39,26 +41,26 @@ gulp.task("icon-font", function() {
                 appendCodePoints: true,
                 fontName: fontName,
                 fontHeight: 1001,
-                formats: ["woff", "ttf", "eot", "svg"],
+                formats: ['woff', 'ttf', 'eot', 'svg'],
                 normalize: true
             })
         )
-        .on("glyphs", function(glyphs, options) {
+        .on('glyphs', function(glyphs, options) {
             // Create the _antwerp-icons-template.scss file based
             // on the template and inject font-name
             // path and CSS class-name
 
-            gulp.src("src/icons/_antwerp-icons-template.scss")
+            gulp.src('src/icons/_antwerp-icons-template.scss')
                 .pipe(
-                    consolidate("lodash", {
-                        className: "icon",
+                    consolidate('lodash', {
+                        className: 'icon',
                         fontName: fontName,
-                        fontPath: "../../fonts",
+                        fontPath: '../../fonts',
                         glyphs: glyphs
                     })
                 )
-                .pipe(rename("_base.antwerpen-icons.scss"))
-                .pipe(gulp.dest("src/styles/base"));
+                .pipe(rename('_base.antwerpen-icons.scss'))
+                .pipe(gulp.dest('src/styles/base'));
         })
-        .pipe(gulp.dest("src/fonts"));
+        .pipe(gulp.dest('src/fonts'));
 });
