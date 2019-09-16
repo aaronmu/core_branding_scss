86221ec967b6c98438fce817012b0bd67a12568f
Tom Ceuppens <t-c@live.be>
2019-09-16 14:44:45 +0200

    fix in minification

--- gulp/iconfont.js
+++ gulp/iconfont.js
@@ -7,49 +7,58 @@
 // - https://www.npmjs.org/package/lodash
 // - https://www.npmjs.org/package/gulp-rename
 
-var gulp = require('gulp');
+var gulp = require("gulp");
 
-var imagemin = require('gulp-imagemin'),
-    iconfont = require('gulp-iconfont'),
-    consolidate = require('gulp-consolidate'),
-    rename = require('gulp-rename');
+var imagemin = require("gulp-imagemin"),
+    iconfont = require("gulp-iconfont"),
+    consolidate = require("gulp-consolidate"),
+    rename = require("gulp-rename"),
+    del = require("del");
 
-gulp.task('icon-font', function() {
-
-    var fontName = 'Antwerpen_Icons';
+gulp.task("icon-font", function() {
+    var fontName = "Antwerpen_Icons";
 
     // Set svg-sources, optimize svg
     // and start creating the font
 
-    return gulp.src('src/icons/*.svg')
-        .pipe(imagemin())
-        .pipe(iconfont({
+    return gulp
+        .src("src/icons/*.svg")
+        .pipe(
+            imagemin([
+                imagemin.svgo({
+                    plugins: [{ convertPathData: false }, { mergePaths: false }]
+                })
+            ])
+        )
+        .pipe(
+            iconfont({
+                // Set file-name for the font and append
+                // codepoints so we always have the same
+                // CSS codes (eg. content: '\e001')
 
-            // Set file-name for the font and append
-            // codepoints so we always have the same
-            // CSS codes (eg. content: '\e001')
-
-            appendCodePoints: true,
-            fontName: fontName,
-            fontHeight: 1001,
-            formats: ['woff', 'ttf', 'eot', 'svg'],
-            normalize:true
-
-        })).on('glyphs', function(glyphs, options) {
+                appendCodePoints: true,
+                fontName: fontName,
+                fontHeight: 1001,
+                formats: ["woff", "ttf", "eot", "svg"],
+                normalize: true
+            })
+        )
+        .on("glyphs", function(glyphs, options) {
             // Create the _antwerp-icons-template.scss file based
             // on the template and inject font-name
             // path and CSS class-name
 
-            gulp.src('src/icons/_antwerp-icons-template.scss')
-                .pipe(consolidate('lodash', {
-                    className: 'icon',
-                    fontName: fontName,
-                    fontPath: '../../fonts',
-                    glyphs: glyphs
-                }))
-                .pipe(rename('_base.antwerpen-icons.scss'))
-                .pipe(gulp.dest('src/styles/base'));
-
+            gulp.src("src/icons/_antwerp-icons-template.scss")
+                .pipe(
+                    consolidate("lodash", {
+                        className: "icon",
+                        fontName: fontName,
+                        fontPath: "../../fonts",
+                        glyphs: glyphs
+                    })
+                )
+                .pipe(rename("_base.antwerpen-icons.scss"))
+                .pipe(gulp.dest("src/styles/base"));
         })
-        .pipe(gulp.dest('src/fonts'));
+        .pipe(gulp.dest("src/fonts"));
 });
