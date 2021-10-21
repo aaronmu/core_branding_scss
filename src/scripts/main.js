095ad42a0a9377c05e669042e833f233deb3e9a0
Sander Geenen <sander.geenen@studiohyperdrive.be>
2021-10-21 13:28:37 +0200

    chore(main.js): set indeterminate state of checkbox in table head

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function () {
-    var checkboxes = document.querySelectorAll("#checkbox2, #checkbox5");
+    var checkboxes = document.querySelectorAll("#checkbox2, #checkbox5, #table-checkbox1");
 
     for (let index = 0; index < checkboxes.length; index++) {
         checkboxes[index].indeterminate = true;
